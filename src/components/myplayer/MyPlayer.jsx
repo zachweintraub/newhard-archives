fbf859ceb2c05bf3c79c3d0ec825850f11f00e2b
Student <example@example.com>
2019-07-30 13:34:30 -0700

    set default player source and fix ymmfsy trailer bug

--- src/components/myplayer/MyPlayer.jsx
+++ src/components/myplayer/MyPlayer.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Player } from 'video-react';
 import '../../../node_modules/video-react/dist/video-react.css';
+import '../myplayer/MyPlayer.css';
 
 const MyPlayer = (props) => {
 
+  console.log(props);
+
+  const defaultSource = () => {
+    let sources = props.content.sources;
+    
+    if(sources.trailer) {
+      return sources.trailer;
+    } else {
+      let outputKey = Object.keys(sources).filter(source => {
+        return sources[source].free;
+      })[0];
+      return sources[outputKey];
+    }
+  }
+
+  const [source, setSource] = useState(defaultSource().uri);
+
+  console.log(source);
+
   return(
-    <Player 
-      poster={props.content.thumbs.player}
-      src={props.content.sources.trailer.uri}
-      controls
-    />
+    <div className='player-wrapper'>
+      <Player 
+        poster={props.content.thumbs.player}
+        src={source}
+        controls
+      />
+    </div>
   );
 }
 
